a6c0b4d2ee5b0b49fdaf29a1939605bee4a8c15b
F4cku23AIO <f4cku23.n@outlook.es>
2022-04-04 23:26:38 -0300

    cambios

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en">
+<html lang="es">
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
@@ -14,129 +14,89 @@
   </head>
   <body>
     <!-- header -->
-    <div class="container">
-      <div class="container">
-        <div class="row">
-          <div class="col">
-            <img src="img/guitarview350.png" alt="">
-          </div>
-          <div class="col">
-            <h1>buscador</h1>
-          </div>
-          <div class="col">
-            <h1>Espacio para menu</h1>
-          </div>
-        </div>
-      </div>
-    </div>
+            <!-- navbar -->
+            <nav class="navbar navbar-expand-lg grid">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#"><img src="img/guitarview350.png" alt="logo"></a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    </button>
+
+                    <div class="collapse navbar-collapse nav justify-content-end" id="navbarNavDropdown">
+                        <ul class="navbar-nav">
+                            
+                            <li class="nav-item">
+                                <input class="btn btn-outline-secondary" type="button" value="Saber mas sobre guitarras" onclick="search()">
+                            </li>
+                                                        
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <!-- fin navbar -->
     
     <!-- slidebar -->
-
-    <div class="container">
-      <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
-        <div class="carousel-indicators">
-          <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-          <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
-          <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
-          <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="3" aria-label="Slide 3"></button>
-          <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="4" aria-label="Slide 4"></button>
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="img/01.jpg" class="d-block w-100" alt="img_slider">
         </div>
-
-        <div class="carousel-inner">
-          <div  class="carousel-item active" data-bs-interval="10000">
-            <img src="img/01.jpg" class="d-block w-30 carousel" alt="...">
-            <div class="carousel-caption d-none d-md-block" >
-              <h5>First slide label</h5>
-              <p>Some representative placeholder content for the first slide.</p>
-            </div>
-          </div>
-          <div class="carousel-item" data-bs-interval="2000">
-            <img src="img/02.jpg" class="d-block w-30 carousel" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-              <h5>Second slide label</h5>
-              <p>Some representative placeholder content for the second slide.</p>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img src="img/03.jpg" class="d-block w-20 carousel" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-              <h5>Third slide label</h5>
-              <p>Some representative placeholder content for the third slide.</p>
-            </div>
-          </div>
-          <div class="carousel-item" data-bs-interval="2000">
-            <img src="img/04.jpg" class="d-block w-30 carousel" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-              <h5>Second slide label</h5>
-              <p>Some representative placeholder content for the second slide.</p>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img src="img/05.jpg" class="d-block w-30 carousel" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-              <h5>Third slide label</h5>
-              <p>Some representative placeholder content for the third slide.</p>
-            </div>
-          </div>
+        <div class="carousel-item">
+          <img src="img/02.jpg" class="d-block w-100" alt="img_slider">
         </div>
-
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
+        <div class="carousel-item">
+          <img src="img/03.jpg" class="d-block w-100" alt="img_slider">
+        </div>
+        <div class="carousel-item">
+          <img src="img/04.jpg" class="d-block w-100" alt="img_slider">
+        </div>
+        <div class="carousel-item">
+          <img src="img/05.jpg" class="d-block w-100" alt="img_slider">
+        </div>
       </div>
     </div>
     
-    <!-- buscadores -->
-    <br>
-    <div class="container">
-      <div class="container">
-        <div class="row">
-          <div class="col col-6">
-            <div class="card" style="width: 18rem;"><br>
-              <div class="container col-12">
-                <input type="text" placeholder="Buscar">
-              </div>              
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-          </div>
-          <div class="col col-6">
-            <div class="card" style="width: 18rem;"><br>
-              <div class="container col-12">
-                <input type="text" placeholder="Buscar">
-              </div>
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-          </div>
-        </div>
+    <!-- cards -->
+    <div  class="row card_comparacion d-flex justify-content-center">
+      
+    <div class="card col col-6" style="width: 25rem;">
+      <img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fbp2.blogger.com%2F_0zKK5jSvkHY%2FSD6u8k7gPoI%2FAAAAAAAAAWE%2FibpmBgvm5zI%2Fw1200-h630-p-k-no-nu%2Fstratocaster-25-aniversario.jpg&f=1&nofb=1" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Fender Stratocaster 25 Aniversario</h5>
+        <p class="card-text">Este modelo de 1979, que conmemora los 25 años de producción de la Strato, lleva una inscripción de aniversario en el cuerpo y clavijas de lujo. </p>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">Cuerdas: 6</li>
+        <li class="list-group-item">A second item</li>
+        <li class="list-group-item">A third item</li>
+      </ul>
+      <div class="card-body">
+        <a href="#" class="card-link">Card link</a>
+        <a href="#" class="card-link">Another link</a>
       </div>
     </div>
 
-
-
-
-
-  <!-- prueba para mostrar commit a Paulo -->
-
-
+    <div class="card col col-6" style="width: 25rem;">
+      <img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F2.bp.blogspot.com%2F-Tqqn3dgYfKw%2FUGaNMNVUbDI%2FAAAAAAAAAB8%2Fg8G8JnEx_4Q%2Fs400%2FGibson-ES0335-1964.jpeg&f=1&nofb=1" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Gibson ES0335 belongs to Eric Clapton</h5>
+        <p class="card-text">En 1964 con precios de $ 847.500. Eric Clapton usa esta guitarra desde 1964, pero rara vez se usa después de que la guitarra se ofrece a un precio de $ 847,500 en una subasta. Este es el precio más alto jamás subastado en una guitarra Gibson en ese momento. </p>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">Cuerdas: 6</li>
+        <li class="list-group-item">A second item</li>
+        <li class="list-group-item">A third item</li>
+      </ul>
+      <div class="card-body">
+        <a href="#" class="card-link">Card link</a>
+        <a href="#" class="card-link">Another link</a>
+      </div>
+    </div>
+    </div>
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
    
-
     <!-- Option 2: Separate Popper and Bootstrap JS -->
     <!--
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
